400a53a29cfc2b808c0c69830761edff795b573d
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-20 20:03:41 +0900

    bump cache for vocal register continuity

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-2-anti-crack';
+const CACHE = 'sound-wave-v20-3-register-continuity';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
