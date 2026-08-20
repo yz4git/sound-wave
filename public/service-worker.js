83994f5fb17b23e73824ac70deb4848501293f29
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-20 17:59:22 +0900

    bump cache for vocal v20.1 presence reset

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-human-phrase-model';
+const CACHE = 'sound-wave-v20-1-presence-reset';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
