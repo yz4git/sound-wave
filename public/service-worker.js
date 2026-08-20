2816e24e994cbc9e39b53e92d28e7de8c9d45bfa
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-21 08:39:06 +0900

    bump cache for vocal v20.8 spectral envelope trajectory

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-7-vocaloid-expression';
+const CACHE = 'sound-wave-v20-8-spectral-envelope-trajectory';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
