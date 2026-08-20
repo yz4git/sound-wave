f367cb2b5b4540bbb58b3da1316d3433a6ea2ada
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-21 00:07:52 +0900

    bump cache for auto compose genre styles

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-6-dynamic-resonance-glottal';
+const CACHE = 'sound-wave-auto-compose-v2-genre-style-engine';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
