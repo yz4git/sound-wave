0d6f27fbb71f502e43e186fcdae65bd045632145
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-20 14:41:38 +0900

    bump cache for natural vocal retune

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v16-vocal-sustain';
+const CACHE = 'sound-wave-v17-vocal-natural';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
