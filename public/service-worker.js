e6738cfd954a7975aa0ebc715c8c1f1f4202b9c0
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-20 22:47:00 +0900

    bump cache for dynamic resonance vocal model

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-5-karaoke-score-phrasing';
+const CACHE = 'sound-wave-v20-6-dynamic-resonance-glottal';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
