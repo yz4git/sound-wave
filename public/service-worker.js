3cdc3c7738075c23f04d7611fd9cd02e69a740e2
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-21 10:06:03 +0900

    bump cache for vocal v20.9

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v20-8-spectral-envelope-trajectory';
+const CACHE = 'sound-wave-v20-9-dynamic-source-filter-coupling';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
