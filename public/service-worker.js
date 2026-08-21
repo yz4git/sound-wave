b79cb0991c2204676336b8dc79e4c5711d6e9f83
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-21 13:34:34 +0900

    bump cache for voice character macros

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'sound-wave-v21-0-producer-tuning-grammar-r2';
+const CACHE = 'sound-wave-v21-1-voice-character-macros';
 const CORE = ['./', './index.html', './manifest.webmanifest', './icon.svg', './vocal-worklet.js'];
 
 self.addEventListener('install', (event) => {
