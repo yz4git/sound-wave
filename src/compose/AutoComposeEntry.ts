98f3520c6859823dff047ed3b2b327f41140a7b0
yz4git <65384484+yz4git@users.noreply.github.com>
2026-08-26 18:27:20 +0900

    wire 8-bit studio into sound wave startup

--- src/compose/AutoComposeEntry.ts
+++ src/compose/AutoComposeEntry.ts
@@ -2,4 +2,5 @@ import './compose.css';
 import './compose-vocal.css';
 import './karaoke-lyrics-panel.css';
 import './KaraokeLyricsPanel';
+import '../eightbit/EightBitEntry';
 export { AutoComposeMode } from './AutoComposeMode';
